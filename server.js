5572baa5c1121654a70f2a2a9c656aec8f7f1c02
jack <kantanop.j@hotmail.com>
2019-06-18 19:28:18 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true });
+fastify.register(require('fastify-cookie'));
+
+const randomNum = async () => {
+    let num = await new Date().getTime()
+    // console.log(num);
+    return num;
+}
+
+// Declare a route
+fastify.post("/api/auth", async (request, reply) => {
+
+    let body = request.body;
+  
+    let _userName = body["username"];
+    let _password = body["password"];
+
+    let sess = await randomNum();
+    console.log(sess)
+    reply.setCookie('Cookie',sess)
+    return sess;
+  
+  })
+  
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
